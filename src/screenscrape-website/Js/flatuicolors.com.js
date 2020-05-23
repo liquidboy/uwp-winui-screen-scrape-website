bbb164cdcf95a5848490feac48e96c0fafa38650
Jose.Fajardo <fajardo_jf@hotmail.com>
2020-05-24 08:50:02 +1000

    demo : reflow UI

--- src/screenscrape-website/Js/flatuicolors.com.js
+++ src/screenscrape-website/Js/flatuicolors.com.js
@@ -25,6 +25,8 @@ if (!!window['injectedFunction'] === false) {
 
     };
     alert('js injected from UWP & function created in webview2');
+} else {
+    alert('js has already been injected into webview2');
 };
 
 // execute injected function
