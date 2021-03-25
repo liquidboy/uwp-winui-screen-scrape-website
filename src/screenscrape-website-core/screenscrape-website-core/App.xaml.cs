56db4ebe195c838f2d9c7bf20bc05d40a4643b6b
Jose.Fajardo <jose.fajardo@oracle.com>
2021-03-26 09:16:24 +1100

    new winui project version

--- src/screenscrape-website-core/screenscrape-website-core/App.xaml.cs
+++ src/screenscrape-website-core/screenscrape-website-core/App.xaml.cs
@@ -19,7 +19,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace screenscape_website_desktop
+namespace screenscrape_website_core
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
