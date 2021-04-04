2feaa5ea06450c8838622292245fbc42bd0893f8
Jose.Fajardo <jose.fajardo@oracle.com>
2021-04-04 15:41:18 +1000

    format converter for numbers/currency

---
+++ src/screenscrape-website-core/screenscrape-website-core/CurrencyConverter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace screenscrape_website_core
+{
+    public class FormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, string language)
+        {
+            string formatString = parameter as string;
+            if (!string.IsNullOrEmpty(formatString))
+            {
+                return string.Format(
+                    new CultureInfo(language), formatString, value);
+            }
+     
+            return value.ToString();
+        }
+
+        // No need to implement converting back on a one-way binding 
+        public object ConvertBack(object value, Type targetType,
+            object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
